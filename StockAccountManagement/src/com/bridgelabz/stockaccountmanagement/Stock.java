5bafe0b8b9662af225cea2e570601ccef0e5f15c
SanketTambe39 <sankettambe39@gmail.com>
2021-07-06 18:58:27 +0530

    ADDED: Initial files

---
+++ StockAccountManagement/src/com/bridgelabz/stockaccountmanagement/Stock.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.stockaccountmanagement;
+
+public class Stock {
+
+}
---
+++ StockAccountManagement/src/com/bridgelabz/stockaccountmanagement/StockMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.stockaccountmanagement;
+
+public class StockMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
